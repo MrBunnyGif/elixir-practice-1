e295de5758760b1ae168b9db3c7fdb0c752dba9b
Pedro Botega <botegapedro@gmail.com>
2024-11-25 16:42:53 +0000

    defdelegate

--- lib/stats.ex
+++ lib/stats.ex
@@ -1,9 +1,11 @@
 defmodule Stats do
-  alias Stats.CentralTendency.Mean
+  alias Stats.CentralTendency.{Mean, Median}
 
   # def population_mean(num), do: Mean.population_mean(num)
   # mesma coisa que a linha de baixo
 
   defdelegate population_mean(numbers), to: Mean
   defdelegate sample_mean(numbers), to: Mean
+
+  defdelegate median(numbers), to: Median
 end
